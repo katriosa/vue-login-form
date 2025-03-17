281df0e0477ae3bd6c60962ff90d2f30bd054115
katriosa <ee.chistyakova@gmail.com>
2025-03-17 16:11:22 +0100

    feat: add sign-up form

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,53 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h2>Loginform</h2>
+  <form class="form-container">
+    <div class="form-title">
+      <h1 class="form-heading">Sign Up</h1>
+      <p>Please fill in this form to create an account.</p>
+    </div>
+    <InputField type="text" placeholder="Your name" minlength="3" autocomplete="given-name" />
+    <div class="button-container">
+      <button type="submit">Sign Up</button>
+    </div>
+  </form>
 </template>
+
+<script setup lang="ts">
+import InputField from '@/components/InputField.vue'
+</script>
+
+<style scoped>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+  width: 50cqi;
+  max-width: 500px;
+  padding: 20px 50px;
+  border-radius: 20px;
+  background-color: var(--color-form);
+  box-shadow: var(--color-box-shadow);
+}
+
+.form-title {
+  text-align: center;
+}
+
+.form-heading {
+  text-shadow: var(--color-text-shadow);
+  margin-bottom: 0.5rem;
+}
+
+.button-container {
+  margin-top: 20px;
+}
+
+@media (max-width: 768px) {
+  .form-container {
+    width: 80cqi;
+    max-width: none;
+    gap: 20px;
+  }
+}
+</style>
---
+++ src/components/InputField.vue
@@ -0,0 +1,40 @@
+<template>
+  <input
+    class="input"
+    :type="type"
+    :placeholder="placeholder"
+    :minlength="minlength"
+    :autocomplete="autocomplete"
+    required
+  />
+</template>
+
+<script setup lang="ts">
+defineProps({
+  type: String,
+  placeholder: String,
+  label: String,
+  minlength: String,
+  autocomplete: String,
+})
+</script>
+
+<style scoped>
+.input {
+  background-color: var(--color-input);
+  outline: none;
+  border: none;
+  padding: 12px 20px;
+  border-radius: 5px;
+  width: 100%;
+  min-width: 200px;
+  max-width: 400px;
+  font-size: inherit;
+}
+
+@media (max-width: 768px) {
+  .input-container {
+    max-width: 80%;
+  }
+}
+</style>
